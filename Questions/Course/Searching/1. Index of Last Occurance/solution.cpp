f6df1451690cbe7e50d8abdbc84d1a0d50ef7988
Sidarth Jaitly <52657841+Sidarth2308@users.noreply.github.com>
2022-04-03 13:20:30 +0000

    Searching question 1

---
+++ Questions/Course/Searching/1. Index of Last Occurance/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	string input;
+	ifstream inputFile("filename.txt");
+
+	while (getline (inputFile, input)) {
+	  cout << myText;
+	}
+	
+  MyFile.close();
+} 
